000d44fc02dbda512546883654bb31c597a30a7d
prince1405 <108205956+prince1405@users.noreply.github.com>
2023-12-30 23:04:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//alert("Hello Everyone, This is 'Prince Gautam' Welcoming You All To My Personal Site Which Makes You Know More About Me And That Will Be Better For You As Well As Me ");
+//
+// $("h1").css("color", "green");
+//
+// $("h1");
+//
+// $("h3").on("mouseover", function() {
+//   $("h3").css("color", "purple");
+// });
